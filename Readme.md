5608d8fd1edf92caf04ebc328635db4ca6573e0e
RedBlaze908 <redblaze212@gmail.com>
2025-12-24 00:23:16 +0100

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,11 @@
+## Template For C++ Games
+This is a template for creating 2D/3D games with C++ with Cmake.
+Feel free to use this repository for your projects!
+
+This repository was inspired (and i also taken some piece of code from: https://github.com/chfhhd/raylibstarter-minimal)
+
+## Library Included
+	- Raylib (Version: 4.5.0) (Window Rendering, sprites ecc..)
+	- Enet (Version: master) (Client & Server, Multiplayer)
+	- Imgui v1.92.4 (Debug menu or menu in general)
+	- rlimgui latest version
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <raylib.h>
+#include <imgui.h>
+#include "library/rlImGui.h"
+#include "config.h"
+
+
+int main() {
+    InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
+    SetTargetFPS(60);
+
+
+    Texture2D myTexture = LoadTexture("assets/graphics/testimage.png");
+
+    rlImGuiSetup(true);
+
+    while (!WindowShouldClose()) {
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        rlImGuiBegin();
+
+        ImGui::Begin("Pannello di Controllo");
+        ImGui::Text("Se vedi questo, rlImGui funziona!");
+        static float colore[3] = { 1.0f, 0.0f, 0.0f };
+        ImGui::ColorEdit3("Colore Sfondo", colore);
+
+        if (ImGui::Button("Reset Finestra")) {
+
+        }
+        ImGui::End();
+
+        rlImGuiEnd();
+
+        DrawText("Hello World", 20, 20, 20, RED);
+        DrawTexture(myTexture, 10, 100, WHITE);
+
+        EndDrawing();
+    }
+
+    UnloadTexture(myTexture);
+
+    CloseWindow();
+
+    return 1;
+}
