82c1d9c2ef05d626d12cacf7e164da8e1f4f6947
RedBlaze908 <redblaze212@gmail.com>
2026-05-06 11:40:22 +0200

    removed rlimgui, it wan't working anymore

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 ﻿#include <raylib.h>
-#include <imgui.h>
-#include "library/rlImGui.h"
 #include "config.h"
 
 
@@ -11,26 +9,11 @@ int main() {
 
     Texture2D myTexture = LoadTexture("assets/graphics/testimage.png");
 
-    rlImGuiSetup(true);
 
     while (!WindowShouldClose()) {
 
         BeginDrawing();
         ClearBackground(WHITE);
-
-        rlImGuiBegin();
-
-        ImGui::Begin("Pannello di Controllo");
-        ImGui::Text("Se vedi questo, rlImGui funziona!");
-        static float colore[3] = { 1.0f, 0.0f, 0.0f };
-        ImGui::ColorEdit3("Colore Sfondo", colore);
-
-        if (ImGui::Button("Reset Finestra")) {
-
-        }
-        ImGui::End();
-
-        rlImGuiEnd();
 
         DrawText("Hello World", 20, 20, 20, RED);
         DrawTexture(myTexture, 10, 100, WHITE);
